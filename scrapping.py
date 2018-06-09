706ec522296b495cafcffacaf289be3ed6a1a524
bc <martooo1936@gmail.com>
2018-06-10 01:56:54 +0200

    scraping implemented

--- scrapping.py
+++ scrapping.py
@@ -10,17 +10,40 @@ sauce = urllib.request.urlopen('https://www.booking.com/').read()
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
 
-# printing the html
-# print(soup.title.string)
+# Booking´s promotions
+def get_promotions():
+    for div in soup.find_all('div', class_='promotion-postcards-list'):
+        a = div.find_all('a')
+        print(a)
 
-# print(soup.find_all('p'))
-
-# for p in soup.find_all('p'):
-#   print(p.string)
-
-""" scrapping for links
-# for ulr in soup.find_all('a'):
-#   print(ulr.get('href'))
-"""
+# get_promotions()
 
 
+# Booking's accommodations on the home page
+def get_accommodations():
+    for div in soup.find_all('div', class_='d-bh-promotion--overflow '):
+        a = div.find_all('a')
+        print(a)
+
+
+# get_accommodations()
+
+# popular destinations
+def get_popular_destinations():
+    for ul in soup.find_all('ul', class_='b-popular_list lp_endorsements_popular_destinations_container'):
+        li = ul.find_all('a')
+        print(li)
+
+
+
+# get_popular_destinations()
+
+
+# Discover with booking
+def discover_destinations():
+    for p in soup.find_all('div', class_='dcbi-country__container'):
+        a = p.find_all('p')
+        print(a)
+
+
+discover_destinations()
